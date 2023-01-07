9c6462d2f3bf1b1450c6d05127ae4ddd0c3f2d2e
Paulo <canalpaulomarques@gmail.com>
2023-01-07 00:12:55 -0300

    adicionando js

---
+++ main.js
@@ -0,0 +1,16 @@
+function ampliar(idImg) {
+    document.querySelector(idImg).style.width = '100%'
+}
+
+var galeria = document.querySelectorAll('.img-galeria')
+
+for (let contador = 0; contador < galeria.length; contador++) {
+    
+    const img = galeria[contador]
+    const idImg = `#img${contador}`
+
+    img.onclick = function () {
+        ampliar(idImg)
+    }
+}
+
